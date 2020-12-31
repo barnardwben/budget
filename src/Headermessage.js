e03cbb81a0d13b5063f33467cf41758939383197
Benjamin Barnard <barnardwben@yahoo.com>
2020-12-31 16:25:46 +0100

    Created Headermessage component

---
+++ src/Headermessage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Headermessage.css";
+const Headermessage = () => {
+  return (
+    <div className="HeaderM-div">
+      <section></section>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut sed fugit
+        ad.
+      </h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, debitis
+        quisquam sapiente esse voluptatum id!
+      </p>
+    </div>
+  );
+};
+
+export default Headermessage;
