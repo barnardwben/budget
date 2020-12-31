df57e19eb52aa4420d8aca9b9c12b31a35347391
Benjamin Barnard <barnardwben@yahoo.com>
2020-12-31 16:26:08 +0100

    Added Headermessage Component to Header component

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Header.css";
+import Headermessage from "./Headermessage";
 import Card from "./Card";
 const Header = () => {
   return (
     <header className="Header-header">
+      <Headermessage />
       <Card />
     </header>
   );
