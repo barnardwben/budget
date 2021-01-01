6bde490cc5f90740f36f010d58d6d8b597bdf34a
Benjamin Barnard <barnardwben@yahoo.com>
2021-01-01 22:15:39 +0100

    Dectructed props from cardobj file

--- src/Card.js
+++ src/Card.js
@@ -1,34 +1,44 @@
 import React from "react";
 import "./Card.css";
 const Card = (props) => {
+  const {
+    idSelector,
+    btype,
+    totrem,
+    itemone,
+    itemtwo,
+    itemthree,
+    itemfour,
+  } = props.card;
+
   return (
-    <div className="Card-body" id={props.id}>
+    <div className="Card-body" id={idSelector}>
       <img className="Card-logo" src="money-bag (1).png" alt="" />
       <h5 className="Card-h5">Budget Type:</h5>
-      <h3 className="Card-h3">Monthly</h3>
+      <h3 className="Card-h3">{btype}</h3>
       <i className="far fa-plus-square"></i>
       <i className="fas fa-edit"></i>
       <div className="Card-main">
         <h4 className="Card-total-budget">Total Remaining Budget:</h4>
-        <h2 className="Card-total-budget-amount">$2,250</h2>
+        <h2 className="Card-total-budget-amount">{totrem}</h2>
         <div className="Card-grid">
           <div className="Card-flex">
-            <h4>Rent: $850</h4>
+            <h4>{itemone}</h4>
             <h4>Spent: $0</h4>
           </div>
           <hr />
           <div className="Card-flex">
-            <h4>Groceries: $250</h4>
+            <h4>{itemtwo}</h4>
             <h4>Spent: $0</h4>
           </div>
           <hr />
           <div className="Card-flex">
-            <h4>Entertainment: $200</h4>
+            <h4>{itemthree}</h4>
             <h4>Spent: $0</h4>
           </div>
           <hr />
           <div className="Card-flex">
-            <h4>Insurance: $450</h4>
+            <h4>{itemfour}</h4>
             <h4>Spent: $0</h4>
           </div>
         </div>
