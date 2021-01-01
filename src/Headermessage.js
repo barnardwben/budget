728cbc753a908cd21e74fb62c449ed953fa6f973
Benjamin Barnard <barnardwben@yahoo.com>
2021-01-01 15:07:55 +0100

    Added Signup component to Headermessage component

--- src/Headermessage.js
+++ src/Headermessage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Headermessage.css";
+import Signup from "./Signup";
 const Headermessage = () => {
   return (
     <div className="HeaderM-div">
@@ -12,6 +13,7 @@ const Headermessage = () => {
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, debitis
         quisquam sapiente esse voluptatum id!
       </p>
+      <Signup />
     </div>
   );
 };
