1d0506e75fe024c0a1a53d6def51d319ba1cbcac
Benjamin Barnard <barnardwben@yahoo.com>
2021-01-01 15:07:04 +0100

    Created Signup button component

---
+++ src/Signup.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Signup.css";
+const Signup = () => {
+  return (
+    <>
+      <button className="signup-btn">Sign Up</button>
+    </>
+  );
+};
+
+export default Signup;
