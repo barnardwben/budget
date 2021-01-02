9e06ddb49d85a60917850b36794ec41868d29ae8
Benjamin Barnard <barnardwben@yahoo.com>
2021-01-02 18:59:58 +0100

    Added Brandname component to Headermessage & Logo components

--- src/Headermessage.js
+++ src/Headermessage.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Headermessage.css";
 import Signup from "./Signup";
+import Brandname from "./Brandname";
 const Headermessage = () => {
   return (
     <div className="HeaderM-div">
-      <section></section>
-      <h1>It’s all coming together</h1>
-      <p>
+      <Brandname />
+      <h1>Finding a safe place for your money</h1>
+      <p className="header-para">
         When you’re on top of your money, life is good. We help you effortlessly
         manage your finances in one place.
       </p>
--- src/Logo.js
+++ src/Logo.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Logo.css";
+import Brandname from "./Brandname";
 
 const Logo = () => {
   return (
     <div>
+      <Brandname id="brandNav" />
       <img className="Logo-img" src="money-bag (1).png" alt="Budget Logo" />
     </div>
   );
