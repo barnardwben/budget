5bf2ae38fa57182505a48aca514553d56ebcbe48
Benjamin Barnard <barnardwben@yahoo.com>
2021-01-02 18:57:31 +0100

    Created Brandname component

---
+++ src/Brandname.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Brandname.css";
+const Brandname = (props) => {
+  return (
+    <>
+      <p className="brand-name" id={props.id}>
+        BudgetBurrow
+      </p>
+    </>
+  );
+};
+
+export default Brandname;
