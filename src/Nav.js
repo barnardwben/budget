8c591313cc22c253fc2fa3dac9b996890ae9c050
Benjamin Barnard <barnardwben@yahoo.com>
2021-01-09 13:26:06 +0100

    Added useEffect and scroll function to Navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Links from "./Links";
 import Signup from "./Signup";
 import Logo from "./Logo";
 import "./Nav.css";
 
 const Nav = () => {
+  // const [scrolling, setScrolling] = useState(false);
+  const scrollTop = 50;
+
+  useEffect(() => {
+    const checkScroll = () => {
+      let currentPosition = window.pageYOffset;
+      if (currentPosition > scrollTop) {
+        console.log("greater");
+        document.getElementById("nav-id").classList.add("navOnScroll");
+      }
+      if (currentPosition <= scrollTop) {
+        console.log("less");
+        document.getElementById("nav-id").classList.remove("navOnScroll");
+      }
+    };
+
+    window.addEventListener("scroll", checkScroll);
+    return () => {
+      console.log("cleanup");
+      window.removeEventListener("scroll", checkScroll);
+    };
+  });
   return (
-    <nav className="Nav-Grid-Container">
+    <nav className="Nav-Grid-Container" id="nav-id">
       <Logo />
       <div>
         <Links link="How it works" />
