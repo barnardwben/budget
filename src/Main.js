5b4015b00fc786ef7b3bc4b374ca3a598e00afaf
Benjamin Barnard <barnardwben@yahoo.com>
2021-01-16 01:21:03 +0100

    Created Main Component, & CSS file

---
+++ src/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "./Card";
+import { cards } from "./cardobj";
+import Header from "./Header";
+import "./Main.css";
+const Main = () => {
+  return (
+    <>
+      <section className="Main-first-section">
+        <img src="\images\mikeslogo.png" alt="" className="mikes-logo" />
+      </section>
+    </>
+  );
+};
+
+export default Main;
