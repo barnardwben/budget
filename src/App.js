64416febc3290c850c97339a42002711aa02c229
Benjamin Barnard <barnardwben@yahoo.com>
2021-01-16 01:03:59 +0100

    Included Main Component in App Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Nav from "./Nav";
 import Header from "./Header";
+import Main from "./Main";
 
 export default function App() {
   return (
     <div className="App">
       <Nav />
       <Header />
+      <Main />
     </div>
   );
 }
